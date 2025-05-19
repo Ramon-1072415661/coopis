68915b5de36ee551b0005da94b40fe34d725102f
lucasGFlores <lucasgabrielflores25@gmail.com>
2025-05-19 17:28:30 -0300

    feat: insert logic of swaping and holding tetrominos in Player class

---
+++ src/Player.java
@@ -0,0 +1,27 @@
+
+public class Player {
+    public Tetromino tetromino;
+    private TetrominoHolder holder;
+    private GetTetrominoLogic next_tetromino_logic;
+
+    public Player(int initialPosition) {
+        holder = new TetrominoHolder(initialPosition,0);
+        next_tetromino_logic = new GetTetrominoLogic(initialPosition, holder);
+        this.getNextTretomino();
+    }
+
+    public void getNextTretomino(){
+        tetromino = next_tetromino_logic.nextTetromino();
+        holder.resetSwap();
+    }
+
+    public void swapTetromino(){
+        tetromino = holder.swap(tetromino);
+    }
+
+    public void insertInHold(){
+        holder.insert(tetromino);
+        this.getNextTretomino();
+    }
+
+}
