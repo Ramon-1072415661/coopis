728b72be0b27b305c63233c4d7a15a2ed6ea9d2e
lucasGFlores <lucasgabrielflores25@gmail.com>
2025-05-19 17:27:58 -0300

    feat: add get next Tetromino logic in GetTetrominoLogic.java

---
+++ src/GetTetrominoLogic.java
@@ -0,0 +1,28 @@
+public class GetTetrominoLogic {
+    private boolean charged = false;
+    private int initialPosition;
+    private TetrominoHolder holder;
+//    private TetrominoQueue queue;
+
+
+    public GetTetrominoLogic(int initialPosition,TetrominoHolder holder) {
+        this.initialPosition = initialPosition;
+        this.holder = holder;
+    }
+
+    public Tetromino nextTetromino() {
+        if(charged && !holder.isEmpty()) {
+            Tetromino holder_tetromino = holder.pop();
+            holder_tetromino.x = initialPosition;
+            return holder_tetromino;
+        }
+        return randomTetromino(); // this is a place holder. Insert Next Queue Logic
+    }
+
+
+    private Tetromino randomTetromino() {
+        Tetromino tetromino = Tetromino.random();
+        tetromino.x = initialPosition;
+        return tetromino;
+    }
+}
