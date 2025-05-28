5ee257c99f2dc9126ce845f1eaed80cd76d13d8c
lucasGFlores <lucasgabrielflores25@gmail.com>
2025-05-27 23:20:05 -0300

    update: holder dont participate from get next tretomino logic

--- src/GetTetrominoLogic.java
+++ src/GetTetrominoLogic.java
@@ -1,27 +1,20 @@
 public class GetTetrominoLogic {
-    private int initialPosition;
-    private TetrominoHolder holder;
+    private int initialXPosition;
 //    private TetrominoQueue queue;
 
 
-    public GetTetrominoLogic(int initialPosition,TetrominoHolder holder) {
-        this.initialPosition = initialPosition;
-        this.holder = holder;
+    public GetTetrominoLogic(int initialXPosition) {
+        this.initialXPosition = initialXPosition;
     }
 
     public Tetromino nextTetromino() {
-        if(!holder.isEmpty()) {
-            Tetromino holder_tetromino = holder.pop();
-            holder_tetromino.x = initialPosition;
-            return holder_tetromino;
-        }
         return randomTetromino(); // this is a place holder. Insert Next Queue Logic
     }
 
 
     private Tetromino randomTetromino() {
         Tetromino tetromino = Tetromino.random();
-        tetromino.x = initialPosition;
+        tetromino.x = initialXPosition;
         return tetromino;
     }
 }
