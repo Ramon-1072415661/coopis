d9dc1efed5474bf4c48d76056da9497c988c7b76
lucasGFlores <lucasgabrielflores25@gmail.com>
2025-05-28 16:08:56 -0300

    chores: add a dysplay to hold the UI components

--- src/PanelDisplay.java
+++ src/PanelDisplay.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
 
-public class PanelDisplay {
+public class PanelDisplay extends JPanel {
+    public PanelDisplay(Player player) {
+        setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+        HolderPanel holder = new HolderPanel(player);
+        Dimension holder_size = holder.getPreferredSize();
+        holder.setMaximumSize(holder_size);
+        holder.setMaximumSize(holder_size);
+        add(holder);
+    }
 }
