70fbd4b0baa94bba663ed7d52e12c7c5273a3e45
lucasGFlores <lucasgabrielflores25@gmail.com>
2025-05-30 21:43:27 -0300

    update: insert holder limit rule

--- src/Board/Controls/Commands/InsertCommand.java
+++ src/Board/Controls/Commands/InsertCommand.java
@@ -6,18 +6,22 @@ import Board.TetrominoLogic.Tetromino;
 import Board.TetrominoLogic.TetrominoHolder;
 import Board.TetrominoLogic.TetrominoResources;
 
+import javax.naming.SizeLimitExceededException;
+
 public class InsertCommand implements Command {
     @Override
     public void execute(Grid grid, Player player) {
         Tetromino tetromino = player.tetromino;
         TetrominoHolder holder = player.getHolder();
+        if (holder.isOnLimit()) return;
         try {
             TetrominoResources resources = TetrominoResources.getInstance();
             int position = resources.tetrominoPosition(tetromino);
             Tetromino tetrominoToHold = new Tetromino(resources.getShapes()[position],tetromino.color);
             holder.insert(tetrominoToHold);
             player.getNextTretomino();
-        } catch (ClassNotFoundException e) {
+        }
+        catch (ClassNotFoundException e) {
             holder.insert(tetromino);
             player.getNextTretomino();
         }
